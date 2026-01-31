56b8f535805f24a6f80cc294f011d8691e965436
KiKaraage <kikaraage@tuta.io>
2026-01-31 04:24:24 +0000

    lm-studio-linux 0.4.1-1

--- Casks/lm-studio-linux.rb
+++ Casks/lm-studio-linux.rb
@@ -1,6 +1,6 @@
 cask "lm-studio-linux" do
-  version "0.3.37-1"
-  sha256 "c0dc931079f18e55ed98bb3dc9cf29eca440257e22e5a2929208c626e8a64894"
+  version "0.4.1-1"
+  sha256 "d18e178cadef7d6798f19e6d41f33a297e26a1d285091cbc30da8252d18a46f0"
 
   url "https://installers.lmstudio.ai/linux/x64/#{version}/LM-Studio-#{version}-x64.AppImage"
   name "LM Studio"
