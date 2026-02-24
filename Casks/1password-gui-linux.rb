bd8b9f1a649a5125eec125b3e375655626d796e1
KiKaraage <kikaraage@tuta.io>
2026-02-24 15:52:35 +0000

    1password-gui-linux 8.12.4

--- Casks/1password-gui-linux.rb
+++ Casks/1password-gui-linux.rb
@@ -2,9 +2,9 @@ cask "1password-gui-linux" do
   arch intel: "x86_64", arm: "aarch64"
   os linux: "linux"
 
-  version "8.11.22"
-  sha256 arm64_linux:  "3e9b0ab8535156dfde616be5999c68eadc8c0bf8147cbac801660fa0a227b6b5",
-         x86_64_linux: "218c021112722fb979a8516f4e77937222954f63d29287aecd0b2231f768995f"
+  version "8.12.4"
+  sha256 arm64_linux:  "6466eea4331781088bf895f98c235aa490b23d4be8d4300e4168a27e7f59a14d",
+         x86_64_linux: "38960d905b348e0576495046706ed9b6d86763cc500fad122378ddc80b9a1415"
 
   arch_suffix =
     case arch
