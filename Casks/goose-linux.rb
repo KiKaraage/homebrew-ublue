18616434b7c90ef6d30eaa24ad4c77f5a3af7196
KiKaraage <kikaraage@tuta.io>
2026-02-28 04:13:58 +0000

    goose-linux 1.26.1

--- Casks/goose-linux.rb
+++ Casks/goose-linux.rb
@@ -1,6 +1,6 @@
 cask "goose-linux" do
-  version "1.19.1"
-  sha256 "ba1711529132e5481d01e37303e9b408339682f722c4ef3b1993d582c54acc1c"
+  version "1.26.1"
+  sha256 "d81123a6d5fd630cf757cef620a9822ccb8e9acc1ccc74c5a4f4ad3661442d19"
 
   url "https://github.com/block/goose/releases/download/v#{version}/Goose-#{version}-1.x86_64.rpm",
       verified: "github.com/block/goose/"
