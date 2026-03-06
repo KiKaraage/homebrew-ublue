1f56a605ffc80e49b5e5b2cfc0e8c0b5d4fd0e68
KiKaraage <kikaraage@tuta.io>
2026-03-06 15:32:31 +0000

    vscodium-linux 1.110.01571

--- Casks/vscodium-linux.rb
+++ Casks/vscodium-linux.rb
@@ -2,9 +2,9 @@ cask "vscodium-linux" do
   arch arm: "arm64", intel: "x64"
   os linux: "linux"
 
-  version "1.107.18627"
-  sha256 arm64_linux:  "df658fef820961520aba19ac322d0416a8d395600ee03802db8a6d9b706a4a98",
-         x86_64_linux: "82a07175de96c359c85e8be2c60b2e222bcb5e7d4ba195cde4d84ae1b2e64a78"
+  version "1.110.01571"
+  sha256 arm64_linux:  "962592414ba65768c22f7855daa3a8a5a10efa41fa274332bce9c4a22a501984",
+         x86_64_linux: "57ec7b3bff8d47f1f375cef81bd06817fbe79ed5ac7fab58058e59595628a1e5"
 
   url "https://github.com/VSCodium/vscodium/releases/download/#{version}/VSCodium-linux-#{arch}-#{version}.tar.gz"
   name "VSCodium"
