6dbed013248ebd8a0b069fda680e4dd87021608e
KiKaraage <kikaraage@tuta.io>
2026-04-15 16:01:38 +0000

    visual-studio-code-linux 1.116.0

--- Casks/visual-studio-code-linux.rb
+++ Casks/visual-studio-code-linux.rb
@@ -2,9 +2,9 @@ cask "visual-studio-code-linux" do
   arch arm: "arm64", intel: "x64"
   os linux: "linux"
 
-  version "1.107.1"
-  sha256 arm64_linux:  "c7f8d65f056179542502d83654e59e0d4df14760e99c70a1213d5281521108ad",
-         x86_64_linux: "a9a19e20dd09c61ec1af7d67d9dec2455004d0fbd35120fe1d24588c123f9474"
+  version "1.116.0"
+  sha256 arm64_linux:  "29047acc3fb79be3a0788092b372e45a8ee40b63b1a85e976b1f014536ba4184",
+         x86_64_linux: "ce87b613dc65a40304e100fc21a82271b023ef5837700f5797299041030524ba"
 
   url "https://update.code.visualstudio.com/#{version}/#{os}-#{arch}/stable"
   name "Microsoft Visual Studio Code"
